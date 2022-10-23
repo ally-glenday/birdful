917f602818396b49bc114dc83386012fe0a89ca7
ally-glenday <alasair-glenday@hotmail.co.uk>
2022-10-23 21:40:40 +0100

    Add all bird images

--- script.js
+++ script.js
@@ -10,38 +10,61 @@ function toggleDropdown() {
 //</audio>
 //<input type="checkbox" id="test-call" onclick="testCall()">
 
-const testAudioCall = document.getElementById("testAudioCall");
-const checkBox = document.getElementById("test-call");
 
-function testCall(){
-  if(checkBox.checked){
-    testAudioCall.play();
+/* <audio id="testAudioCall"><source src="bird_calls/blue-tit.mp3" type="audio/mpeg">
+                  Sorry, but it appears your browser does not support the audio element.
+                  </audio>
+                <input type="checkbox" id="test-call" onclick="testCall()"> */
+
+
+// document.getElementsByTagName("audio").style.display = "none"; 
+
+/* Blue Tit Call */
+const blueTitAudio = document.getElementById("blue-tit-audio"); 
+const blueTitCheckbox = document.getElementById("blue-tit-checkbox");
+
+function playBlueTitCall(){
+  if(blueTitCheckbox.checked){
+    blueTitAudio.play();
   }
   else{
-    testAudioCall.pause();  
+    blueTitAudio.pause();  
+  }
+}
+/* Great Tit Call */
+const greatTitAudio = document.getElementById("great-tit-audio");
+const greatTitCheckbox = document.getElementById("great-tit-checkbox");
+
+function playGreatTitCall(){
+  if(greatTitCheckbox.checked){
+    greatTitAudio.play();
+  }
+  else{
+    greatTitAudio.pause();  
   }
 }
 
 
 
 
-/* Blue Tit Call */ 
-  var blueTitCall = document.getElementById("blue-tit-call");
-  function playBlueTitCall(){
-    blueTitCall.play();
-  }
-  function pauseBlueTitCall(){
-    blueTitCall.pause();
-  }
 
-  /* Great Tit Call */ 
-  var greatTitCall = document.getElementById("great-tit-call");
-  function playGreatTitCall(){
-    greatTitCall.play();
-  }
-  function pauseGreatTitCall(){
-    greatTitCall.pause();
-  }
+// /* Blue Tit Call */ 
+//   var blueTitCall = document.getElementById("blue-tit-call");
+//   function playBlueTitCall(){
+//     blueTitCall.play();
+//   }
+//   function pauseBlueTitCall(){
+//     blueTitCall.pause();
+//   }
+
+//   /* Great Tit Call */ 
+//   var greatTitCall = document.getElementById("great-tit-call");
+//   function playGreatTitCall(){
+//     greatTitCall.play();
+//   }
+//   function pauseGreatTitCall(){
+//     greatTitCall.pause();
+//   }
   
 
 
