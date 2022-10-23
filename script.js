fa08340854a76241cffd5c1a890da2cc012d6d24
ally-glenday <alasair-glenday@hotmail.co.uk>
2022-10-23 13:33:48 +0100

    Checkbox (JS) Call working!!!

--- script.js
+++ script.js
@@ -1,23 +1,116 @@
 function toggleDropdown() {
     document.getElementById("myDropdown").classList.toggle("show");
+}
+
+
+
+
+//<audio id="testAudioCall"><source src="bird_calls/blue-tit.mp3" type="audio/mpeg">
+//Sorry, but it appears your browser does not support the audio element.
+//</audio>
+//<input type="checkbox" id="test-call" onclick="testCall()">
+
+const testAudioCall = document.getElementById("testAudioCall");
+const checkBox = document.getElementById("test-call");
+
+function testCall(){
+  if(checkBox.checked){
+    testAudioCall.play();
   }
+  else{
+    testAudioCall.pause();  
+  }
+}
 
 
 
 
-
-
-  
+/* Blue Tit Call */ 
   var blueTitCall = document.getElementById("blue-tit-call");
-
   function playBlueTitCall(){
     blueTitCall.play();
-    
   }
-
   function pauseBlueTitCall(){
     blueTitCall.pause();
   }
+
+  /* Great Tit Call */ 
+  var greatTitCall = document.getElementById("great-tit-call");
+  function playGreatTitCall(){
+    greatTitCall.play();
+  }
+  function pauseGreatTitCall(){
+    greatTitCall.pause();
+  }
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //  <audio id="blue-tit-audio"><source src="bird_calls/blue-tit.mp3" type="audio/mpeg">
+  //     Sorry, but it appears your browser does not support the audio element.
+  //     </audio>
+  //   <input id="blue-tit-checkbox" onclick="playBlueTitCall" type="checkbox">
+
+
+
+
+
+  // <input type="checkbox" id="cena" onchange="myfunction(this)" />
+  // <label for="cena"></label>
+ 
+    //  var audio = new Audio('rusbtaudio.mp3');
+    //  audio.oncanplay = function() {
+    //  if (document.getElementById("cena").checked) this.play()
+    //  }
+    //  function myfunction(el) {    
+    //    if (el.checked) {
+    //      audio.load();
+    //    } else {
+    //      audio.pause(); //pause audio
+    //    }
+     
+
+
+
+
+//   var blueTitCheckbox = document.getElementById("blue-tit-checkbox");
+//   var blueTitAudio = document.getElementById("blue-tit-audio");
+  
+//  function playBlueTitCall(){
+//   blueTitAudio.play();
+//  }
+  
+  
+
   
 
 
