f83124236e39c7ec1949105d300ca7986da46a74
ally-glenday <alasair-glenday@hotmail.co.uk>
2022-11-01 09:21:37 +0000

    Template changes

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-// function toggleDropdown() {
-//     document.getElementById("myDropdown").classList.toggle("show");
-// }
-
-
-
-
-//<audio id="testAudioCall"><source src="bird_calls/blue-tit.mp3" type="audio/mpeg">
-//Sorry, but it appears your browser does not support the audio element.
-//</audio>
-//<input type="checkbox" id="test-call" onclick="testCall()">
-
-
-/* <audio id="testAudioCall"><source src="bird_calls/blue-tit.mp3" type="audio/mpeg">
-                  Sorry, but it appears your browser does not support the audio element.
-                  </audio>
-                <input type="checkbox" id="test-call" onclick="testCall()"> */
-
-
-// document.getElementsByTagName("audio").style.display = "none"; 
-
 /* Blue Tit Call */
 const blueTitAudio = document.getElementById("blue-tit-audio"); 
 const blueTitCheckbox = document.getElementById("blue-tit-checkbox");
@@ -26,7 +5,6 @@ const blueTitCheckbox = document.getElementById("blue-tit-checkbox");
 function playBlueTitCall(){
   if(blueTitCheckbox.checked){
     blueTitAudio.play();
-    
   }
   else{
     blueTitAudio.pause();  
@@ -39,7 +17,6 @@ const greatTitCheckbox = document.getElementById("great-tit-checkbox");
 function playGreatTitCall(){
   if(greatTitCheckbox.checked){
     greatTitAudio.play();
-    
   }
   else{
     greatTitAudio.pause();  
@@ -51,24 +28,6 @@ function playGreatTitCall(){
 
 
 
-// /* Blue Tit Call */ 
-//   var blueTitCall = document.getElementById("blue-tit-call");
-//   function playBlueTitCall(){
-//     blueTitCall.play();
-//   }
-//   function pauseBlueTitCall(){
-//     blueTitCall.pause();
-//   }
-
-//   /* Great Tit Call */ 
-//   var greatTitCall = document.getElementById("great-tit-call");
-//   function playGreatTitCall(){
-//     greatTitCall.play();
-//   }
-//   function pauseGreatTitCall(){
-//     greatTitCall.pause();
-//   }
-  
 
 
 
@@ -96,46 +55,6 @@ function playGreatTitCall(){
 
 
 
-
-
-
-
-
-  //  <audio id="blue-tit-audio"><source src="bird_calls/blue-tit.mp3" type="audio/mpeg">
-  //     Sorry, but it appears your browser does not support the audio element.
-  //     </audio>
-  //   <input id="blue-tit-checkbox" onclick="playBlueTitCall" type="checkbox">
-
-
-
-
-
-  // <input type="checkbox" id="cena" onchange="myfunction(this)" />
-  // <label for="cena"></label>
- 
-    //  var audio = new Audio('rusbtaudio.mp3');
-    //  audio.oncanplay = function() {
-    //  if (document.getElementById("cena").checked) this.play()
-    //  }
-    //  function myfunction(el) {    
-    //    if (el.checked) {
-    //      audio.load();
-    //    } else {
-    //      audio.pause(); //pause audio
-    //    }
-     
-
-
-
-
-//   var blueTitCheckbox = document.getElementById("blue-tit-checkbox");
-//   var blueTitAudio = document.getElementById("blue-tit-audio");
-  
-//  function playBlueTitCall(){
-//   blueTitAudio.play();
-//  }
-  
-  
 
   
 
