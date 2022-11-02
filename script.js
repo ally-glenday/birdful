9b5b514b960b9341217c6f2f9241cadc05f140d0
ally-glenday <alasair-glenday@hotmail.co.uk>
2022-11-02 11:30:59 +0000

    Great Tit & Blue Tit working

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 /* Blue Tit Call */
-const blueTitAudio = document.getElementById("blue-tit-audio"); 
-const blueTitCheckbox = document.getElementById("blue-tit-checkbox");
+var blueTitAudio = document.getElementById("blue-tit-audio"); 
+var blueTitCheckbox = document.getElementById("blue-tit-checkbox");
 
 function playBlueTitCall(){
   if(blueTitCheckbox.checked){
     blueTitAudio.play();
+    blueTitLabel.innerHTML("Hey");
+    
   }
   else{
     blueTitAudio.pause();  
+    blueTitLabel.innerHTML("Ho");
   }
-}
+} 
+ 
+
 /* Great Tit Call */
 const greatTitAudio = document.getElementById("great-tit-audio");
 const greatTitCheckbox = document.getElementById("great-tit-checkbox");
@@ -22,6 +27,8 @@ function playGreatTitCall(){
     greatTitAudio.pause();  
   }
 }
+
+
 
 
 
