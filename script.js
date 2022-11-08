c417f8735e7f33bf7730c34350e3ec7eda62856f
ally-glenday <alasair-glenday@hotmail.co.uk>
2022-11-08 12:37:08 +0000

    Add all calls to JS

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /* Blue Tit Call */
-var blueTitAudio = document.getElementById("blue-tit-audio"); 
-var blueTitCheckbox = document.getElementById("blue-tit-checkbox");
+const blueTitAudio = document.getElementById("blue-tit-audio"); 
+const blueTitCheckbox = document.getElementById("blue-tit-checkbox");
 
 function playBlueTitCall(){
   if(blueTitCheckbox.checked){
@@ -11,7 +11,6 @@ function playBlueTitCall(){
   }
 } 
  
-
 /* Great Tit Call */
 const greatTitAudio = document.getElementById("great-tit-audio");
 const greatTitCheckbox = document.getElementById("great-tit-checkbox");
@@ -26,6 +25,19 @@ function playGreatTitCall(){
 }
 
 /* Coal Tit Call */
+const coalTitAudio = document.getElementById("coal-tit-audio");
+const coalTitCheckbox = document.getElementById("coal-tit-checkbox");
+
+function playCoalTitCall(){
+  if(coalTitCheckbox.checked){
+    coalTitAudio.play();
+  }
+  else{
+    coalTitAudio.pause();  
+  }
+}
+
+/* Long Tailed Tit Call */
 const longTailedTitAudio = document.getElementById("long-tailed-tit-audio");
 const longTailedTitCheckbox = document.getElementById("long-tailed-tit-checkbox");
 
@@ -129,10 +141,257 @@ function playGreenfinchCall(){
   }
 }
 
+/* Chaffinch Call */
+const chaffinchAudio = document.getElementById("chaffinch-audio");
+const chaffinchCheckbox = document.getElementById("chaffinch-checkbox");
+
+function playChaffinchCall(){
+  if(chaffinchCheckbox.checked){
+    chaffinchAudio.play();
+  }
+  else{
+    chaffinchAudio.pause();  
+  }
+}
+
+/* Blackbird Call */
+const blackbirdAudio = document.getElementById("blackbird-audio");
+const blackbirdCheckbox = document.getElementById("blackbird-checkbox");
+
+function playBlackbirdCall(){
+  if(blackbirdCheckbox.checked){
+    blackbirdAudio.play();
+  }
+  else{
+    blackbirdAudio.pause();  
+  }
+}
+
+/* Wren Call */
+const wrenAudio = document.getElementById("wren-audio");
+const wrenCheckbox = document.getElementById("wren-checkbox");
+
+function playWrenCall(){
+  if(wrenCheckbox.checked){
+    wrenAudio.play();
+  }
+  else{
+    wrenAudio.pause();  
+  }
+}
+
+/* Pied Wagtail Call */
+const piedWagtailAudio = document.getElementById("pied-wagtail-audio");
+const piedWagtailCheckbox = document.getElementById("pied-wagtail-checkbox");
+
+function playPiedWagtailCall(){
+  if(piedWagtailCheckbox.checked){
+    piedWagtailAudio.play();
+  }
+  else{
+    piedWagtailAudio.pause();  
+  }
+}
+
+/* Ring Necked Parakeet Call */
+const ringNeckedParakeetAudio = document.getElementById("ring-necked-parakeet-audio");
+const ringNeckedParakeetCheckbox = document.getElementById("ring-necked-parakeet-checkbox");
+
+function playRingNeckedParakeetCall(){
+  if(ringNeckedParakeetCheckbox.checked){
+    ringNeckedParakeetAudio.play();
+  }
+  else{
+    ringNeckedParakeetAudio.pause();  
+  }
+}
+
+/* Great Spotted Woodpecker Call */
+const greatSpottedWoodpeckerAudio = document.getElementById("great-spotted-woodpecker-audio");
+const greatSpottedWoodpeckerCheckbox = document.getElementById("great-spotted-woodpecker-checkbox");
+
+function playGreatSpottedWoodpeckerCall(){
+  if(greatSpottedWoodpeckerCheckbox.checked){
+    greatSpottedWoodpeckerAudio.play();
+  }
+  else{
+    greatSpottedWoodpeckerAudio.pause();  
+  }
+}
+
+/* Nuthatch Call */
+const nuthatchAudio = document.getElementById("nuthatch-audio");
+const nuthatchCheckbox = document.getElementById("nuthatch-checkbox");
+
+function playNuthatchCall(){
+  if(nuthatchCheckbox.checked){
+    nuthatchAudio.play();
+  }
+  else{
+    nuthatchAudio.pause();  
+  }
+}
+
+/* Treecreeper Call */
+const treecreeperAudio = document.getElementById("treecreeper-audio");
+const treecreeperCheckbox = document.getElementById("treecreeper-checkbox");
+
+function playTreecreeperCall(){
+  if(treecreeperCheckbox.checked){
+    treecreeperAudio.play();
+  }
+  else{
+    treecreeperAudio.pause();  
+  }
+}
+
+/* Green Woodpecker Call */
+const greenWoodpeckerAudio = document.getElementById("green-woodpecker-audio");
+const greenWoodpeckerCheckbox = document.getElementById("green-woodpecker-checkbox");
+
+function playGreenWoodpeckerCall(){
+  if(greenWoodpeckerCheckbox.checked){
+    greenWoodpeckerAudio.play();
+  }
+  else{
+    greenWoodpeckerAudio.pause();  
+  }
+}
+
+/* Jay Call */
+const jayAudio = document.getElementById("jay-audio");
+const jayCheckbox = document.getElementById("jay-checkbox");
+
+function playJayCall(){
+  if(jayCheckbox.checked){
+    jayAudio.play();
+  }
+  else{
+    jayAudio.pause();  
+  }
+}
+
+/* Goldcrest Call */
+const goldcrestAudio = document.getElementById("goldcrest-audio");
+const goldcrestCheckbox = document.getElementById("goldcrest-checkbox");
+
+function playGoldcrestCall(){
+  if(goldcrestCheckbox.checked){
+    goldcrestAudio.play();
+  }
+  else{
+    goldcrestAudio.pause();  
+  }
+}
+
+/* Sparrowhawk Call */
+const sparrowhawkAudio = document.getElementById("sparrowhawk-audio");
+const sparrowhawkCheckbox = document.getElementById("sparrowhawk-checkbox");
+
+function playSparrowhawkCall(){
+  if(sparrowhawkCheckbox.checked){
+    sparrowhawkAudio.play();
+  }
+  else{
+    sparrowhawkAudio.pause();  
+  }
+}
+
+/* Kestrel Call */
+const kestrelAudio = document.getElementById("kestrel-audio");
+const kestrelCheckbox = document.getElementById("kestrel-checkbox");
+
+function playKestrelCall(){
+  if(kestrelCheckbox.checked){
+    kestrelAudio.play();
+  }
+  else{
+    kestrelAudio.pause();  
+  }
+}
+
+/* Hobby Call */
+const hobbyAudio = document.getElementById("hobby-audio");
+const hobbyCheckbox = document.getElementById("hobby-checkbox");
+
+function playHobbyCall(){
+  if(hobbyCheckbox.checked){
+    hobbyAudio.play();
+  }
+  else{
+    hobbyAudio.pause();  
+  }
+}
+
+/* Mallard Call */
+const mallardAudio = document.getElementById("mallard-audio");
+const mallardCheckbox = document.getElementById("mallard-checkbox");
+
+function playMallardCall(){
+  if(mallardCheckbox.checked){
+    mallardAudio.play();
+  }
+  else{
+    mallardAudio.pause();  
+  }
+}
+
+/* Mute Swan Call */
+const muteSwanAudio = document.getElementById("mute-swan-audio");
+const muteSwanCheckbox = document.getElementById("mute-swan-checkbox");
+
+function playMuteSwanCall(){
+  if(muteSwanCheckbox.checked){
+    muteSwanAudio.play();
+  }
+  else{
+    muteSwanAudio.pause();  
+  }
+}
+
+/* Kingfisher Call */
+const kingfisherAudio = document.getElementById("kingfisher-audio");
+const kingfisherCheckbox = document.getElementById("kingfisher-checkbox");
+
+function playKingfisherCall(){
+  if(kingfisherCheckbox.checked){
+    kingfisherAudio.play();
+  }
+  else{
+    kingfisherAudio.pause();  
+  }
+}
+
+/* Great Crested Grebe Call */
+const greatCrestedGrebeAudio = document.getElementById("great-crested-grebe-audio");
+const greatCrestedGrebeCheckbox = document.getElementById("great-crested-grebe-checkbox");
+
+function playGreatCrestedGrebeCall(){
+  if(greatCrestedGrebeCheckbox.checked){
+    greatCrestedGrebeAudio.play();
+  }
+  else{
+    greatCrestedGrebeAudio.pause();  
+  }
+}
+
+/* Little Grebe Call */
+const littleGrebeAudio = document.getElementById("little-grebe-audio");
+const littleGrebeCheckbox = document.getElementById("little-grebe-checkbox");
+
+function playLittleGrebeCall(){
+  if(littleGrebeCheckbox.checked){
+    littleGrebeAudio.play();
+  }
+  else{
+    littleGrebeAudio.pause();  
+  }
+}
 
 
 
 
+/* Fallback for users without JS enabled */ 
 
 
 // let audioWithControls = document.getElementsByTagName("audio");
