b4c0c72b820d63ef80b20a15d6ce6ef92222cc22
ally-glenday <alasair-glenday@hotmail.co.uk>
2022-11-10 19:30:14 +0000

    Updated footer and header php

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -1,3 +1,11 @@
-<footer>
+<!-- <footer>
     <p>Copyright Birdful <? echo date("Y"); ?></p>
-</footer>
+</footer> -->
+        <footer>
+            <ul>
+            <li><a href="about_birdful.html">About</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">Creative Commons</a></li>
+            </ul>
+            <p>Copyright Birdful <? echo date("Y"); ?></p>
+        </footer>
--- includes/header.inc.php
+++ includes/header.inc.php
@@ -1,93 +1,147 @@
 <header>
-    <div class="dropdown">
-    <button onclick="toggleDropdown()" class="dropbtn">Search by Colour</button>
-    <div id="myDropdown" class="dropdown-content">
-    <!--     <input type="text" placeholder="Search by colour" id="myInput" onkeyup="filterFunction()"> -->
+    <noscript>JavaScript is disabled. Please turn on JavaScript for full functionality</noscript>
+  
+  
+    <nav id="main-navigation">
+    
+     
+    <div aria-label="Find a bird by colour menu" class="dropdown main-nav-flex-item">
+  
+      <input type="checkbox" class="toggle-dropdown" id="toggle-dropdown">
+      <label for="toggle-dropdown" class="toggle-dropdown">
+        
+      </label>
+      
+      <div id="myDropdown" class="dropdown-content">
         <div class="main-menu">
-        <div id="colour-block" class="sub-menu">
-            <a href="#">Grey</a>
+          <div class="sub-menu">
+            <p>Grey</p>
             <div class="sub-menu-content">
-            <a href="#">Collard Dove</a>
-            <a href="#">Wood Pigeon</a>
-            <a href="#">Kestrel</a>
-            <a href="#">Coal Tit</a>
-            </div> <!--Closing sub menu div--> 
-            
-            
-        
-            
+              <a href="wood_pigeon.html">Wood Pigeon</a>
+              <a href="kestrel.html">Kestrel</a>
+              <a href="coal_tit.html">Coal Tit</a>
+              <a href="sparrowhawk.html">Sparrowhawk</a>
+            </div>
+            <!--Closing sub menu div-->
+  
+  
             <div class="sub-menu">
-            <a href="#about">Blue</a>
-            <div class="sub-menu-content">
-                <a href="#base">Blue Tit</a>
-                <a href="#base">Great Tit</a>
-                <a href="#base">Jay</a>
-                <a href="#base">Magpie</a>
-                <a href="#base">Nuthatch</a>
-                <a href="#base">Kingfisher</a>
-            </div> <!--Closing sub menu div--> 
-            
-            <div class="sub-menu">  <!--Opening sub menu div--> 
-            <a href="#about">Green</a>
-            <div class="sub-menu-content">
-            <a href="#base">Ring Necked Parakeet</a>
-            <a href="#base">Greenfinch</a>
-            <a href="#base">Green Woodpecker</a>
-        
-            </div> <!--Closing sub menu div--> 
-            
-            
+              <p>Blue</p>
+              <div class="sub-menu-content">
+                <a href="blue_tit.html">Blue Tit</a>
+                <a href="great_tit.html">Great Tit</a>
+                <a href="jay.html">Jay</a>
+                <a href="magpie.html">Magpie</a>
+                <a href="nuthatch.html">Nuthatch</a>
+                <a href="kingfisher.html">Kingfisher</a>
+                <a href="sparrowhawk.html">Sparrowhawk</a>
+              </div>
+              <!--Closing sub menu div-->
+  
+              <div class="sub-menu">
+                <!--Opening sub menu div-->
+                <p>Green</p>
+                <div class="sub-menu-content">
+                  <a href="ring_necked_parakeet.html">Ring Necked Parakeet</a>
+                  <a href="greenfinch.html">Greenfinch</a>
+                  <a href="green_woodpecker.html">Green Woodpecker</a>
+                  <a href="great_tit.html">Great Tit</a>
+                  <a href="mallard.html">Mallard</a>
+                  
+  
+                </div>
+                <!--Closing sub menu div-->
+  
+  
+  
+                <div class="sub-menu">
+                  <!--Opening sub menu div-->
+                  <p>Black</p>
+                  <div class="sub-menu-content">
+                    <a href="blackbird.html">Blackbird</a>
+                    <a href="great_spotted_woodpecker.html">Great Spotted Woodpecker</a>
+                    <a href="magpie.html">Magpie</a>
+                    <a href="little_grebe.html">Little Grebe</a>
+                  </div>
+                  <!--Closing sub menu div-->
+  
+                  <div class="sub-menu">
+                    <!--Opening sub menu div-->
+                    <p>White</p>
+                    <div class="sub-menu-content">
+                      <a href="mute_swan.html">Mute Swan</a>
+                      <a href="pied_wagtail.html">Pied Wagtail</a>
+                      <a href="long_tailed_tit.html">Long Tailed Tit</a>
+                      <a href="little_grebe.html">Little Grebe</a>
+                      <a href="magpie.html">Magpie</a>
+                    </div>
+                    <!--Closing sub menu div-->
+  
+                    <div class="sub-menu">
+                      <!--Opening sub menu div-->
+                      <p>Orange | Red</p>
+                      <div class="sub-menu-content">
+                        <a href="robin.html">Robin</a>
+                        <a href="chaffinch.html">Chaffinch</a>
+                        <a href="hobby.html">Hobby</a>
+                        <a href="goldfinch.html">Goldfinch</a>
+                      </div>
+                      <!--Closing sub menu div-->
+  
+                      <div class="sub-menu">
+                        <!--Opening sub menu div-->
+                        <p>Yellow | Gold</p>
+                        <div class="sub-menu-content">
+                          <a href="goldcrest.html">Goldcrest</a>
+                          <a href="goldfinch.html">Goldfinch</a>
+                          <a href="blue_tit.html">Blue Tit</a>
+                          <a href="great_tit.html">Great Tit</a>
+  
+                        </div>
+                        <!--Closing sub menu div-->
+  
+                        <div class="sub-menu">
+                          <!--Opening sub menu div-->
+                          <p>Brown</p>
+                          <div class="sub-menu-content">
+                            <a href="house_sparrow.html">House Sparrow</a>
+                            <a href="tree_sparrow.html">Tree Sparrow</a>
+                            <a href="wren.html">Wren</a>
+                            <a href="treecreeper.html">Treecreeper</a>
+                            <a href="jay.html">Jay</a>
+                          </div>
+                          <!--Closing sub menu div-->
+  
+  
+                        </div>
+                        <!--Closing main menu div-->
+                      </div>
+                    </div>
+                  </div>
+                  <!--Closing '.dropdown' div-->
+          
+                 
+                </nav>
+                <a class="main-nav-flex-item" id="temp-home-button" href="index.html"></a>
                 
-            <div class="sub-menu"> <!--Opening sub menu div--> 
-            <a href="#">Black</a>
-            <div class="sub-menu-content">
-            <a href="#base">Blackbird</a>
-            <a href="#base">Crow</a>
-            <a href="#base">Great Spotted Woodpecker</a>
-            <a href="#base">Little Grebe</a>
-            </div> <!--Closing sub menu div--> 
-            
-            <div class="sub-menu"> <!--Opening sub menu div--> 
-            <a href="#">White</a>
-            <div class="sub-menu-content">
-            <a href="#base">Mute Swan</a>
-            <a href="#base">Pied Wagtail</a>
-            <a href="#base">Long Tailed Tit</a>
-            <a href="#base">Little Grebe</a>
-            </div> <!--Closing sub menu div--> 
-            
-            <div class="sub-menu"> <!--Opening sub menu div--> 
-            <a href="#about">Red | Orange</a>
-            <div class="sub-menu-content">
-            <a href="#base">Robin</a>
-            <a href="#base">Chaffinch</a>
-            <a href="#base">Hobby</a>
-            <a href="#base">Moorhen</a>
-            <a href="#base">Goldfinch</a>
-            </div> <!--Closing sub menu div--> 
+                  
                 
-            <div class="sub-menu"> <!--Opening sub menu div--> 
-            <a href="#about">Yellow | Gold</a>
-            <div class="sub-menu-content">
-            <a href="#base">Goldcrest</a>
-            <a href="#base">Goldfinch</a>
-            <a href="#base">Blue Tit</a>
-            <a href="#base">Great Tit</a>
-            
-            </div> <!--Closing sub menu div--> 
                 
-            <div class="sub-menu"> <!--Opening sub menu div--> 
-            <a href="#about">Brown</a>
-            <div class="sub-menu-content">
-            <a href="#base">House Sparrow</a>
-            <a href="#base">Tree Sparrow</a>
-            <a href="#base">Wren</a>
-            <a href="#base">Treecreeper</a>
-            </div> <!--Closing sub menu div--> 
-            
-            
-        </div> <!--Closing main menu div--> 
-        </div>
-    </div>
-    </div> <!--Closing '.dropdown' div--> 
-</header>
+                <!-- <nav class="main-navigation">
+                  
+                  <input type="checkbox" id="nav-toggle" class="nav-toggle">
+                  <div class="nav-toggle-container">
+                    <ul>
+                      <li><a href="#">Home</a></li>
+                      <li><a href="#">About</a></li>
+                      <li><a href="#">Bird ID</a></li>
+                      <li><a href="#">Get Birding</a></li>
+                    </ul>
+                  </div>
+              
+                <label for="nav-toggle" class= "nav-toggle-label">
+                  <span></span>
+                </label>
+               </nav> -->
+                  
+  </header>
